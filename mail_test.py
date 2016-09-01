a0870bd9bb0547a390f74aecf125608ad8ae63f0
Soumyadeep Roy <soumya.ace9@rediffmail.com>
2016-09-01 11:45:10 +0530

    Added program description and workflow details.

--- mail_test.py
+++ mail_test.py
@@ -1,3 +1,14 @@
+'''
+This program demonstrates how you can send a mail from a Python program and the commented part at the end shows how yo can use
+a loop structure to send the same message multiple times to the same user.
+Steps :
+1. We first login to my gmail account using the in-App password that could be generated in Gmail.
+2. We use the gmail server to send the message using SMTP protocol, through its corresponding port no. This details vary across 
+the different mail services.
+3. After logging in, we send the single or multiple times and then log out.
+NOTE : These codes are strictly for educational purposes. Use them sensibly.
+'''
+
 import smtplib
 import time
 
@@ -23,7 +34,7 @@ smtpObj.sendmail('soumyadeep.roy9@gmail.com', 'soumya.ace9@rediffmail.com', mess
 print 'Mails Sent successfully(Buggy code)'
 
 '''
-#Used only for sending the same message multiple times(for educational purposes only)
+#Used only for sending the same message multiple times
 for spamming in range(1,20):
 	print("Spam mail no.{0} sent").format(spamming)
 	smtpObj.sendmail('soumyadeep.roy9@gmail.com', 'soumya.ace9@rediffmail.com', message)
