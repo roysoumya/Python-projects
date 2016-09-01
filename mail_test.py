13e3fc3204a3afee2bc8c6be06863b0173decf76
Soumyadeep Roy <soumya.ace9@rediffmail.com>
2016-09-01 10:29:18 +0530

    Changed the password and Commented the spamming loop

--- mail_test.py
+++ mail_test.py
@@ -5,7 +5,7 @@ smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
 
-password = 'ovzkcwskaaelhxyc'
+password = 'app-password'
 message = '''From: Soumyadeep Roy <soumyadeep.roy9@gmail.com>
 To: Soumya Roy <soumya.ace9@rediffmail.com>
 Subject: Freshers Orientation Python Mail test SNB-222
@@ -18,14 +18,16 @@ Best regards,
 Soumyadeep Roy
 Kalyani Government Engineering College
 '''
-smtpObj.login('soumyadeep.roy9@gmail.com', 'ovzkcwskaaelhxyc')
+smtpObj.login('soumyadeep.roy9@gmail.com', password)
 smtpObj.sendmail('soumyadeep.roy9@gmail.com', 'soumya.ace9@rediffmail.com', message)
 print 'Mails Sent successfully(Buggy code)'
 
+'''
+#Used only for sending the same message multiple times(for educational purposes only)
 for spamming in range(1,20):
 	print("Spam mail no.{0} sent").format(spamming)
 	smtpObj.sendmail('soumyadeep.roy9@gmail.com', 'soumya.ace9@rediffmail.com', message)
 	time.sleep(1.0)
-
+'''
 smtpObj.quit()
 
