3c41deb079acbaa689d2b1ee3d034afd09819dab
Soumyadeep Roy <soumya.ace9@rediffmail.com>
2016-09-01 18:29:47 +0530

    Added program description and workflow details.

--- reverse_geocoding_large.py
+++ reverse_geocoding_large.py
@@ -1,6 +1,6 @@
 '''
 This program is used to reverse-geocode a large number of lat-long pairs. Reverse-geocoding means given a latitude-longitude
-pair, generate the corresponding location's geographical details.
+pair, generate the corresponding location's geographical details like name of the place, district, state and country code.
 Steps :
 1.The list of lat-long pairs are generated using the nested for-loops, where each grid covers a spatial projection of 2.5 x 2.5 
 degrees.
